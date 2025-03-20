8174d338e50aebe18e3ebb367f26d2e3546f6fb1
szywat <szywat04@gmail.com>
2025-03-20 19:08:25 +0100

    +testAddNeg,testMultOneByNeg,testMultOneByZero

--- src/test/java/kalkulator/CalculatorTest.java
+++ src/test/java/kalkulator/CalculatorTest.java
@@ -23,4 +23,29 @@ public class CalculatorTest {
 		sut.mult(2);
 		assertEquals("1*2 = 2", 2, sut.getState());
 	}
+
+	@Test
+	public void testAddNeg() {
+		Calculator sut = new Calculator();
+		sut.setState(1);
+		sut.add(-1);
+		assertEquals("1+(-1) = 0", 0, sut.getState());
+	}
+
+	@Test
+	public void testMultOneByNeg() {
+		Calculator sut = new Calculator();
+		sut.setState(1);
+		sut.mult(-1);
+		assertEquals("1*(-1) = -1", -1, sut.getState());
+	}
+
+	@Test
+	public void testMultOneByZero() {
+		Calculator sut = new Calculator();
+		sut.setState(1);
+		sut.mult(0);
+		assertEquals("1*0 = 0", 0, sut.getState());
+	}
+
 }
