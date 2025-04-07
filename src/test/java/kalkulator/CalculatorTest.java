08ce230b44ede15d7d9c204c363715faec58a18a
szywat <szywat04@gmail.com>
2025-04-08 01:37:32 +0200

    +test wielomian

--- src/test/java/kalkulator/CalculatorTest.java
+++ src/test/java/kalkulator/CalculatorTest.java
@@ -2,7 +2,10 @@ package kalkulator;
 
 import org.junit.*;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CalculatorTest {
 	@Test
@@ -81,14 +84,6 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testDivByZero() {
-		Calculator sut = new Calculator();
-		sut.setState(1);
-		sut.div(0);
-		assertEquals("1 / 0 = 1 (nie zmieniamy wartości przez error)", 1, sut.getState());
-	}
-
-	@Test
 	public void testSaveMemory() {
 		Calculator sut = new Calculator();
 		sut.setState(1);
@@ -121,4 +116,68 @@ public class CalculatorTest {
 		assertEquals("(M 0)-1 = (M -1)", -1, sut.getMem());
 	}
 
+	@Test
+	public void testDivByZero() {
+		Calculator sut = new Calculator();
+		sut.setState(1);
+		sut.div(0);
+        assertTrue("1 / 0 = err", sut.getError());
+	}
+
+	@Test
+	public void testWielomian(){
+			int[] tablica = new int[6];
+		Calculator sut = new Calculator();
+		int[] x = {-3, -2, -1, 1, 2, 3};
+		for (int i = 0; i < x.length; i++) {
+			sut.setState(x[i]);
+			sut.power(5);
+			sut.saveMem();
+
+			sut.setState(x[i]);
+			sut.power(4);
+			sut.mult(4);
+			sut.addMem();
+
+			sut.setState(x[i]);
+			sut.power(3);
+			sut.mult(3);
+
+			sut.addMem();
+
+			sut.setState(x[i]);
+			sut.power(2);
+			sut.mult(2);
+			sut.subMem();
+
+			sut.setState(17);
+			sut.addMem();
+
+			int licznik = sut.getMem();
+
+			sut.setState(x[i]);
+			sut.power(2);
+			sut.saveMem();
+
+			sut.setState(x[i]);
+			sut.mult(-7);
+			sut.addMem();
+
+			sut.setState(1);
+			sut.addMem();
+
+			int mianownik = sut.getMem();
+
+			sut.setState(licznik);
+			sut.div(mianownik);
+
+			int wynik = sut.getState();
+
+			tablica[i] = wynik;
+		}
+		assertEquals(Arrays.toString(new int[]{0, 0, 1, -4, -14, -58}), Arrays.toString(tablica));
+	}
 }
+
+// wielomian = (x^5 + 4x^4 + 3x^3 - 2x^2 + 17) / (x^2 -7x + 1)
+// kalkulator online: ~0, ~0, ~1, ~-4, ~-14, ~-58
